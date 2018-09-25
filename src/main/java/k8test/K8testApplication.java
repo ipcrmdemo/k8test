26789cc204df03f06989da2c280c6a4846058473
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 09:14:06 -0400

    Initial commit from Atomist

---
+++ src/main/java/k8test/K8testApplication.java
@@ -0,0 +1,12 @@
+package k8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8testApplication.class, args);
+	}
+}
---
+++ src/main/java/k8test/K8testController.java
@@ -0,0 +1,20 @@
+package k8test;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class K8testController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello, world! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
